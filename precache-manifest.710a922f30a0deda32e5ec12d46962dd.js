8db560de027734dbf3b38ab00e2c5743faffcac8
Moonsoo Jo <jojuno@ucla.edu>
2020-07-03 21:14:38 -0700

    Updates

---
+++ precache-manifest.710a922f30a0deda32e5ec12d46962dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8767a01fb440d5e617a12f7e59852774",
+    "url": "/dota-api/index.html"
+  },
+  {
+    "revision": "f649706398f711ba5533",
+    "url": "/dota-api/static/css/main.a94c00dc.chunk.css"
+  },
+  {
+    "revision": "693a24797663a0c98ec7",
+    "url": "/dota-api/static/js/2.0e1a82a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dota-api/static/js/2.0e1a82a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f649706398f711ba5533",
+    "url": "/dota-api/static/js/main.c4e870eb.chunk.js"
+  },
+  {
+    "revision": "5220177b21ab2de3fb4e",
+    "url": "/dota-api/static/js/runtime-main.b1e17090.js"
+  },
+  {
+    "revision": "7bfccbf4584cbf49815e2c4a5448d539",
+    "url": "/dota-api/static/media/dota 2.7bfccbf4.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dota-api/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "574c670bb6ab981a77812be16e9af393",
+    "url": "/dota-api/static/media/minimap_hero_sheet.574c670b.png"
+  }
+]);
